749267c84a325dc7fdf5653ded1faf938e773fa6
Michelle Hung <chialinghung74@gmail.com>
2022-07-03 23:40:17 +0800

    add exist code

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool IsPalindrome(int x)
+{
+    if (x < 0)
+    {
+        return false;
+    }
+    var stringArray = x.ToString().ToCharArray();
+    var isEqual = stringArray.SequenceEqual(stringArray.Reverse());
+    return isEqual;
+}
+
+var isPalindrome = IsPalindrome(-9999999);
+Console.WriteLine(isPalindrome);
---
+++ Valid Parentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool IsValid(string s)
+{
+    var strings = s.ToCharArray();
+    var dictionary = new Dictionary<string, string>()
+    {
+        {"(", ")"},
+        {"[", "]"},
+        {"{", "}"},
+    };
+    if (strings.Length % 2 != 0)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < strings.Length; i++)
+    {
+        if (dictionary.TryGetValue(strings[i].ToString(), out var value) && value == strings[i + 1].ToString())
+        {
+            i++;
+            continue;
+        }
+
+        if (dictionary.TryGetValue(strings[i+1].ToString(), out var value2) && value2 == strings[i + 2].ToString())
+        {
+            i = i + 2;
+            continue;
+        }
+        
+        if (dictionary.TryGetValue(strings[i+1].ToString(), out var _))
+        {
+            i = i + 2;
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+
+Console.WriteLine(IsValid("{[]}"));
